febacf73c45874020c03096f11d152ff7ff94592
Hoang Duc <notspring09@gmail.com>
2020-04-17 11:15:56 +0700

    add all entities

---
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -0,0 +1,21 @@
+﻿using eShopSolution.Data.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext( DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get ; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
